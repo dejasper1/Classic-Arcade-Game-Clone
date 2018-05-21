fc7aff0678f7956df64cdc6f9d809778bac97e84
Dylan Jasper <dylanejasper@gmail.com>
2018-05-20 19:38:21 -0700

    project commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Classic Arcade Game Clone
+
+## Description 
+This is a game project inspired by the classic Frogger game. The gameplay consists of a player controlled character, multiple enemies, and a tile grid. The objective is to guide the character to the top of the grid (water tiles) without colliding with an enemy. 10 points are awarded for each move upward and 100 points for reaching the water tiles. After reaching the water tile or colliding with an enemy the player position is reset. When the player runs out of attempts a modal with the score displayed is presented with an option to play again.
+
+The visual assets and game loop engine (found in the engine.js file) were provided by Udacity.  
+
+## Instructions 
+Somply click the link to the live demo and use the arrow keys to control the movement of the game character. 
+
+## Built With 
+* [Bulma](https://bulma.io/) - The CSS framework used for the modal
+
+## License
+This project is licensed under the MIT License
+
+## Live Demo
---
+++ js/app.js
@@ -0,0 +1,141 @@
+let score = 0;
+
+class Enemy {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = 'images/enemy-bug.png';
+    }
+    // Update the enemy's position
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        this.x += this.speed * dt;
+        if (this.x > 505) {
+            this.x = -25;
+            this.speed = 100 + Math.floor(Math.random() * 200);
+        }
+        // check for player collision 
+        if (this.y === player.y && this.x >= player.x - 50 && this.x <= player.x + 50) {
+            // player position coordinates are returned to start and counter is decreased
+            gameStats.update();
+            player.x = 202;
+            player.y = 400;
+
+        }
+    }
+    // "Draw" the enemy on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+}
+
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/char-boy.png';
+    }
+    update() {}
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    handleInput(keypress) {
+        if (keypress === 'left' && this.x > 0) {
+            console.log(this.x)
+            this.x -= 101;
+        }
+        if (keypress === 'right' && this.x < 375) {
+            console.log(this.y)
+            this.x += 101;
+        }
+        if (keypress === 'up' && this.y > 0) {
+            this.y -= 83;
+            score += 10;
+        }
+        if (keypress === 'down' && this.y < 375) {
+            this.y += 83;
+        }
+        if (this.y <= 0) {
+            setTimeout(() => {
+                player.x = 202;
+                player.y = 400;
+                score += 100;
+            }, 400);
+        }
+    }
+
+}
+
+class Stats {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/Heart.png'
+        this.hearts = 3;
+    }
+    update() {
+        this.hearts -= 1;
+    }
+    // ctx.drawImage(image, dx, dy, dWidth, dHeight);
+    render() {
+        let heartTotal = this.hearts;
+        let heartPosition = this.x;
+        if (heartTotal === 0) {
+            displayModal();
+
+        }
+        for (let i = 0; i < heartTotal; i++) {
+            ctx.drawImage(Resources.get(this.sprite), heartPosition, this.y, 30, 50);
+            heartPosition -= 30;
+        }
+    }
+
+}
+
+
+// enemies array
+const allEnemies = [];
+// enemy location array
+const enemyLocation = [68, 151, 234];
+// instantiate enemy object with location
+enemyLocation.forEach((locationY) => {
+    enemy = new Enemy(0, locationY, 200);
+    allEnemies.push(enemy);
+});
+// instantiate player object
+const player = new Player(202, 400);
+
+// instantiate stats object
+const gameStats = new Stats(470, 05);
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function (e) {
+    const allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    // call the handleInput method on the player object 
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+
+// modal display
+
+function displayModal() {
+    const modal = document.querySelector('.modal');
+    const canvas = document.querySelector('canvas');
+    const modalBody = document.querySelector('.modal-score');
+    const button = document.querySelector('.button');
+    canvas.parentNode.removeChild(canvas);
+    modal.classList.toggle('is-active');
+    modalBody.textContent = score;
+    button.addEventListener('click', () => {
+        location.reload();
+    });
+}
